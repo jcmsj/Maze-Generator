a1cb51883e25280e42a678112ea95aff377ec9f3
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-11-19 13:26:37 +0800

    Add DFS now returns traversal order

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,15 +1,19 @@
 from random_dfs import Cell, State, as_matrix, import_maze_details, parse_cli_args
 
 def depth_first_search(maze: list[list[Cell]], start:Cell, end:Cell):
-    """Run depth first search on a matrix representation of the maze"""
+    """Run depth first search on a matrix representation of the maze. Returns a tuple containing the path and the entire traversal order"""
     stack = [(start, [(start.X, start.Y)])]
     visited = set()
-
+    traversal_order = []
     while stack:
         current, path = stack.pop()
 
-        if (current.X, current.Y) == (end.X, end.Y):
-            return path
+        coord = (current.X, current.Y)
+        # Every loop, add the current cell's coordinate to the traversal list
+        traversal_order.append(coord)
+
+        if coord == (end.X, end.Y):
+            return path, traversal_order
 
         if current not in visited:
             visited.add(current)
@@ -19,7 +23,7 @@ def depth_first_search(maze: list[list[Cell]], start:Cell, end:Cell):
                 next_cell = maze[current.Y + neighbor.value[1]][current.X + neighbor.value[0]]
                 stack.append((next_cell, path + [(next_cell.X, next_cell.Y)]))
 
-    return None
+    return None, traversal_order
 
 def print_path(path):
     if path:
@@ -32,8 +36,10 @@ def dfs_main():
     if args.file:
         maze_info = import_maze_details(args.file)
         maze_matrix = as_matrix(maze_info['graph'])
-        path = depth_first_search(maze_matrix, maze_info['start'], maze_info['end'])
+        path, traversal = depth_first_search(maze_matrix, maze_info['start'], maze_info['end'])
         print_path(path)
+        print_path(traversal)
+
 
 if __name__ == '__main__':
     dfs_main()
